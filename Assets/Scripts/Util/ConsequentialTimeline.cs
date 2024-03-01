bc7b29b4f146fc620a0348f9bc058c419665188a
GFunction <ryan.mm.taylor@gmail.com>
2024-03-01 12:31:23 -0800

    Update ConsequentialTimeline.cs

--- Assets/Scripts/Util/ConsequentialTimeline.cs
+++ Assets/Scripts/Util/ConsequentialTimeline.cs
@@ -8,15 +8,31 @@ using UnityEngine;
 #nullable enable
 namespace GFunction.ConsequentialTimeline
 {
+    /// <summary>
+    /// The entirety of this class works on the assumption that every implementation of <see cref="ITimelineAction{TState}"/> and <see cref="ITimelineEvaluator{TState, TInput}"/> are 100% clean.
+    /// </summary>
+    /// <typeparam name="TState"></typeparam>
+    /// <typeparam name="TInput"></typeparam>
     public class ConsequentialTimeline<TState, TInput> where TState : class
     {
         public delegate void EvaluatorModifier(ref ITimelineEvaluator<TState, TInput> evaluator);
         public delegate IEnumerable<ITimelineEvaluator<TState, TInput>> ConsequenceAdder(in ITimelineAction<TState> action);
 
-        private ConsequentialTimelineNode<TState> _head;
+        /// <summary>
+        /// Volitile. Proper clearing/removing of added function is the responsibility of user.
+        /// </summary>
+        public GuardedCollectionHandle<ConsequenceAdder> ConsequenceListeners => new(_consequenceAdders);
+        /// <summary>
+        /// Volitile. Proper clearing/removing of added function is the responsibility of user.
+        /// </summary>
+        public GuardedCollectionHandle<EvaluatorModifier> EvaluatorListeners => new(_evaluatorModifiers);
+
+
+        private ConsequenceSequence<TState> _head;
         private TState _state;
         private List<ConsequenceAdder> _consequenceAdders;
         private List<EvaluatorModifier> _evaluatorModifiers;
+        private List<ConsequentialTimeline<TState, TInput>> _branches;
 
         public ConsequentialTimeline(TState infoState, ITimelineAction<TState> head)
         {
@@ -24,20 +40,37 @@ namespace GFunction.ConsequentialTimeline
             _head = new(head, null, 0, _state);
             _consequenceAdders = new();
             _evaluatorModifiers = new();
+            _branches = new();
+        }
+        //branch constructor
+        private ConsequentialTimeline(ConsequentialTimeline<TState, TInput> source, ConsequenceSequence<TState> head)
+        {
+            _state = source._state;
+            _head = head;
+            _consequenceAdders = new(source._consequenceAdders);
+            _evaluatorModifiers = new(source._evaluatorModifiers);
+            _branches = new();
         }
     }
-    public class ConsequentialTimelineNode<TState> where TState : class
+
+    public class TimelineNode<TState, TInput> where TState : class
+    {
+        private ITimelineEvaluator<TState, TInput> _evaluator;
+        private List<ConsequenceSequence<TState>> _branches;
+        private TimelineNode<TState, TInput> _from;
+    }
+    public class ConsequenceSequence<TState> where TState : class
     {
         public int LeftForward => _consequences.Count - _pointer;
         public int LeftBackward => _pointer + 1;
-        public ConsequentialTimelineNode<TState>? Parent => _parent;
+        public ConsequenceSequence<TState>? Parent => _parent;
 
-        internal T _value;
+        internal ITimelineAction<TState> _cause;
         internal TState _state;
         internal int _pointer = -1;
         internal int _depth;
-        internal ConsequentialTimelineNode<TState>? _parent;
-        internal List<ConsequentialTimelineNode<TState>> _consequences;
+        internal ConsequenceSequence<TState>? _parent;
+        internal List<ConsequenceSequence<TState>> _consequences;
 
         public void AddConsequence(ITimelineAction<TState> consequence)
         {
@@ -49,7 +82,7 @@ namespace GFunction.ConsequentialTimeline
             if (LeftForward <= 0) return false;
             if (_pointer == -1)
             {
-                _value.Forward(ref _state);
+                _cause.Forward(ref _state);
                 _pointer++;
             }
             for (int i = _pointer; i < LeftForward; i++)
@@ -67,37 +100,26 @@ namespace GFunction.ConsequentialTimeline
                 _consequences[_pointer].BackwardAll();
             }
             Debug.Assert(_pointer == 0);
-            _value.Backward(ref _state);
+            _cause.Backward(ref _state);
             _pointer--;
             if (_parent is not null) _parent._pointer--;
             return true;
         }
 
-        internal ConsequentialTimelineNode(ITimelineAction<TState> value, ConsequentialTimelineNode<TState>? parent, int depth, TState state)
+        internal ConsequenceSequence(ITimelineAction<TState> cause, ConsequenceSequence<TState>? parent, int depth, TState state)
         {
-            _value = value;
+            _cause = cause;
             _parent = parent;
             _depth = depth;
             _consequences = new();
             _state = state;
         }
     }
-
-    internal struct TimelineEvaluation<TState, TInput> where TState : class
-    {
-        public ITimelineEvaluator<TState, TInput> EvaluatedFrom;
-        public ITimelineAction<TState> Action;
-
-        internal TimelineEvaluation(ITimelineEvaluator<TState, TInput> evaluatedFrom, ITimelineAction<TState> action)
-        {
-            EvaluatedFrom = evaluatedFrom;
-            Action = action;
-        }
-    }
     public interface ITimelineAction<TState> where TState : class
     {
         public abstract void Forward(ref TState state);
         public abstract void Backward(ref TState state);
+        public abstract bool Equivalent(ITimelineAction<TState> other);
     }
     public interface ITimelineEvaluator<TState, TInput> where TState : class
     {
