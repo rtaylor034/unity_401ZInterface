25994673a93a9a1c01c6ac2bff2090a067593130
GFunction <ryan.mm.taylor@gmail.com>
2024-04-02 10:29:15 -0700

    Result

--- Assets/Scripts/Util/GStructures.cs
+++ Assets/Scripts/Util/GStructures.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 #nullable enable
@@ -12,29 +13,67 @@ namespace GStructures
     {
         public NahException() : base("NahException reached. It's literally over.") { }
     }
-    public abstract class Option<T>
+    public abstract class Option<T> 
     {
-        private static Exception _unrecognizedException => new NotSupportedException("Unwrapped a class that extends 'Option', but isnt 'None' or 'Some' (why the fuck are we extending 'Option'?)");
-        public sealed class None : Option<T> { }
         public sealed class Some : Option<T>
         {
-            public T Value;
+            public T Value { get; }
             public Some(T value)
             {
                 Value = value;
             }
         }
-        public T Unwrap() => this switch
+        public sealed class None : Option<T> { }
+        public T Expect(string msg) => this switch
         {
             Some value => value.Value,
-            None => throw new Exception("Unwrapped a 'None' Option."),
-            _ => throw _unrecognizedException
+            _ => throw new Exception(msg)
         };
+        public T Unwrap() => Expect($"Unwrapped a {GetType().Name} {GetType().BaseType.Name}.");
         public T UnwrapOrElse(Func<T> evaluate) => this switch
         {
             Some value => value.Value,
-            None => evaluate(),
-            _ => throw _unrecognizedException
+            _ => evaluate()
+        };
+        public Option<T> Map(Func<T, T> func) => this switch
+        {
+            Some some => new Some(func(some.Value)),
+            _ => this
+        };
+    }
+    public abstract class Result<T, E>
+    {
+        public sealed class Ok : Result<T, E>
+        {
+            public T Value { get; }
+            public Ok(T value) => Value = value;
+        }
+        public sealed class Err : Result<T, E>
+        {
+            public E Value { get; }
+            public Err(E value) => Value = value;
+        }
+        public Result<E, T> Invert() => this switch
+        {
+            Ok ok => new Result<E, T>.Err(ok.Value),
+            Err err => new Result<E, T>.Ok(err.Value),
+            _ => throw new System.NotSupportedException()
+        };
+        public T Expect(string msg) => this switch
+        {
+            Ok value => value.Value,
+            _ => throw new Exception(msg)
+        };
+        public T Unwrap() => Expect($"Unwrapped a {GetType().Name} {GetType().BaseType.Name}.");
+        public T UnwrapOrElse(Func<T> evaluate) => this switch
+        {
+            Ok value => value.Value,
+            _ => evaluate()
+        };
+        public Result<T, E> Map(Func<T, T> func) => this switch
+        {
+            Ok ok => new Ok(func(ok.Value)),
+            _ => this
         };
     }
 }
