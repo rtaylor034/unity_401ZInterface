8e1b8b65cc42acc2c59821d59a39a746856ed970
GFunction <ryan.mm.taylor@gmail.com>
2024-05-16 10:08:45 -0700

    Update FourZeroOne.cs

--- Assets/Scripts/TheLanguage/FourZeroOne.cs
+++ Assets/Scripts/TheLanguage/FourZeroOne.cs
@@ -25,8 +25,22 @@ namespace FourZeroOne
     }
     public interface IOutputInterface
     {
+        /// <summary>
+        /// Called when a token is transformed via rules, <paramref name="pairs"/> holding the transformation steps. <br></br>
+        /// <i><see cref="WriteToken(IToken)"/> is only called on the final resulting token.</i>
+        /// </summary>
+        /// <param name="pairs"></param>
         public void WriteRuleSteps(IEnumerable<(IToken fromToken, Rule.IRule rule)> pairs);
+        /// <summary>
+        /// Called when <paramref name="token"/> *starts* resolving.<br></br>
+        /// <i>Only called on final realization after all rules are applied.</i>
+        /// </summary>
+        /// <param name="token"></param>
         public void WriteToken(IToken token);
+        /// <summary>
+        /// Called when a token resolves to <paramref name="resolution"/>.
+        /// </summary>
+        /// <param name="resolution"></param>
         public void WriteResolution(IOption<ResObj>? resolution);
     }
     
