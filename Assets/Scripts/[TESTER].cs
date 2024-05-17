a7d0ee51f53297bae02756226744096fa1d620f6
GFunction <ryan.mm.taylor@gmail.com>
2024-05-17 12:10:22 -0700

    update

--- Assets/Scripts/[TESTER].cs
+++ Assets/Scripts/[TESTER].cs
@@ -93,33 +93,7 @@ public class TESTER : MonoBehaviour
     // Update is called once per frame
     async void Update()
     {
-        /*
-        var token1 = MakeToken.tRecursive<r.Number, r.Multi<r.Number>, r.Number>(new()
-        {
-            A = 0.tConst(),
-            B = 0.Sequence(x => x + 1).Take(5).Map(x => x.tConst()).tToMulti(),
-            RecursiveProxyStatement = P =>
-                P.pSubEnvironment(RHint<r.Number>.Specify(), new()
-                {
-                    EnvironmentProxies = new()
-                    {
-                        P.pOriginalB().pAs(out var pool),
-                        P.pOriginalA().pAs(out var counter)
-                    },
-                    SubProxy = P.pOriginalA().pIsGreaterThan(2.tConst().pAsProxyFor(P)).pIfTrue(RHint<r.Number>.Specify(), new()
-                    {
-                        Then = counter.tRef().pAsProxyFor(P),
-                        Else = P.pRecurseWith(new()
-                        {
-                            A = counter.tRef().tAdd(1.tConst()).pAsProxyFor(P),
-                            B = pool.tRef().tWithout(counter.tRef().tYield()).pAsProxyFor(P)
-                        }).pAdd(counter.tRef().pAsProxyFor(P))
-                    })
-                })
-        });
-        await token1.ResolveWithRules(_program);
-        //Debug.Log(await token1.ResolveWithRules(_program));
-        */
+        
     }
 }
 
